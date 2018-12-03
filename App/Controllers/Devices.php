f8f8ef7e751adcc04bff4f3634356fe67a8377e2
Joshua Arco <joshuarco@gmail.com>
2018-12-03 04:46:19 -0800

    omg its over!

--- App/Controllers/Devices.php
+++ App/Controllers/Devices.php
@@ -10,16 +10,41 @@ use Core\View;
 
 class Devices extends Authenticated
 {
+	public function myLightAction(): void
+	{
+		
+		$id = $this->route_params['id'];
+		
+		$devices = $this->parseDevices();
+		
+		$things = [];
+		foreach ($devices as $key => $value){
+			$things[] = ['id' => $key, 'name' => $value['name']];
+		}
+		
+		$light = $devices[$id];
+		$light['id'] = $id;
+		
+		View::renderTemplate('Devices/my_light.html.twig', [
+			'light' => $light,
+			'url' => Config::IOT_API.'/api/cmd/'.$id,
+		]);
+	}
+	
 	public function myDevicesAction(): void
 	{
-		$devices = file_get_contents(Config::IOT_API.'/api/query');
-
-		$devices = json_decode($devices);
-		$devices = json_decode(json_encode( $devices[1]), true);
+		$devices = $this->parseDevices();
+		
+		$things = [];
+		foreach ($devices as $key => $value){
+			$things[] = ['id' => $key, 'name' => $value['name']];
+		}
 //		var_dump($devices);
 //		exit;
 		
-		View::renderTemplate('Devices/my_devices.html.twig', $devices);
+		View::renderTemplate('Devices/my_devices.html.twig', [
+			'devices' => $things
+		]);
 	}
 	
 	public function philipsLightAction(): void
@@ -34,16 +59,6 @@ class Devices extends Authenticated
 		$devices = json_decode($devices);
 		$devices = json_decode(json_encode( $devices[1]), true);
 		
-		foreach ($devices as $name => $caps)
-		{
-			var_dump('name: ' . $name);
-			foreach ($caps as $settings_name => $setting){
-				var_dump('    setting name: ' . $settings_name);
-//				foreach ($setting as $)
-			}
-		}
-
-//	    var_dump($devices['dummy-light1']['caps']['bri']);
-//	    exit;
+		return $devices;
 	}
 }
