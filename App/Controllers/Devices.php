2ffde35b2bc09e5932d891fc47df59c032d9f129
Joshua Arco <joshuarco@gmail.com>
2019-01-27 21:56:34 -0800

    forgotten push?

--- App/Controllers/Devices.php
+++ App/Controllers/Devices.php
@@ -7,9 +7,23 @@ namespace App\Controllers;
 
 use App\Config;
 use Core\View;
+use function GuzzleHttp\Promise\is_settled;
 
 class Devices extends Authenticated
 {
+	
+	public function lightOnOffAction(): void
+	{
+	
+	}
+	
+	/**
+	 * takes in an 'id' as its route_params
+	 *
+	 * @throws \Twig_Error_Loader
+	 * @throws \Twig_Error_Runtime
+	 * @throws \Twig_Error_Syntax
+	 */
 	public function myLightAction(): void
 	{
 		
@@ -17,13 +31,11 @@ class Devices extends Authenticated
 		
 		$devices = $this->parseDevices();
 		
-		$things = [];
-		foreach ($devices as $key => $value){
-			$things[] = ['id' => $key, 'name' => $value['name']];
-		}
-		
 		$light = $devices[$id];
 		$light['id'] = $id;
+		
+//		var_dump($light['caps']['power']);
+//		exit;
 		
 		View::renderTemplate('Devices/my_light.html.twig', [
 			'light' => $light,
@@ -39,8 +51,6 @@ class Devices extends Authenticated
 		foreach ($devices as $key => $value){
 			$things[] = ['id' => $key, 'name' => $value['name']];
 		}
-//		var_dump($devices);
-//		exit;
 		
 		View::renderTemplate('Devices/my_devices.html.twig', [
 			'devices' => $things
@@ -60,5 +70,12 @@ class Devices extends Authenticated
 		$devices = json_decode(json_encode( $devices[1]), true);
 		
 		return $devices;
+	}
+	
+	public function getDevice(string $id)
+	{
+		$device = file_get_contents(Config::IOT_API.'/cmd/'.$id.'/query');
+		
+		return $device[$id] ?? null;
 	}
 }
