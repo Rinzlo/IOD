4abfedab8e0e720b4579c55852d133da518a4523
Rinzlo <joshuarco@gmail.com>
2019-03-20 18:12:21 -0700

    Updates everything

--- App/Mail.php
+++ App/Mail.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace App;
 
@@ -7,15 +7,15 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 class Mail
 {
-	
-	/**
-	 * Send an email
-	 * @param string $to
-	 * @param string $subject
-	 * @param string $text
-	 * @param string $html
-	 * @throws \PHPMailer\PHPMailer\Exception
-	 */
+
+    /**
+     * Send an email
+     * @param string $to
+     * @param string $subject
+     * @param string $text
+     * @param string $html
+     * @throws \PHPMailer\PHPMailer\Exception
+     */
     public static function send(string $to, string $subject, string $text, string $html)
     {
         //Create a new PHPMailer instance
@@ -59,14 +59,14 @@ class Mail
 
         //Read an HTML message body from an external file, convert referenced images to embedded,
         //convert HTML into a basic plain-text alternative body
-//        $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+        //        $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
         //Replace the plain text body with one created manually
         // body stuff
         $mail->msgHTML($html);
         $mail->AltBody = $text;
 
         //Attach an image file
-//        $mail->addAttachment('images/phpmailer_mini.png');
+        //        $mail->addAttachment('images/phpmailer_mini.png');
 
         //send the message, check for errors
         if (!$mail->send()) {
